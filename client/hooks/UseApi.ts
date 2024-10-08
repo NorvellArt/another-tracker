cbc44a3a24e12cb4ab70cf4b4e46e926f459b518
Artem Batorshin <a.batorshin@gandiva.ru>
2024-10-08 22:16:23 +0300

    try api host

--- client/hooks/UseApi.ts
+++ client/hooks/UseApi.ts
@@ -1,7 +1,7 @@
 import { AuthClientStore } from "../clientAPI/Token";
 import { ApiMethod } from "../types/api";
 
-const apiUrl = "http://45.140.169.11:80/";
+const apiUrl = "http://api:80/";
 
 const sendRequest = (
     method: ApiMethod,
